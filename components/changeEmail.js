a2386c56107899953791166dd0561471bc4ee5a9
Rajath Rao <raorajath1998@gmail.com>
2023-04-24 12:43:12 +0530

    add new components

---
+++ components/changeEmail.js
@@ -0,0 +1,74 @@
+'use strict';
+var credImport=require('./cred.js')
+ 
+module.exports = {
+  metadata: () => ({
+    name: 'changeEmail',
+    properties: {
+      newAdd: { required: true, type: 'string' },
+      code: { required: true, type: 'string' },
+    },
+    supportedActions: ['success', 'fail']
+  }),
+
+  /**
+   * invoke methods gets called when the custom component state is executed in the dialog flow
+   * @param {CustomComponentContext} context 
+   */
+  invoke: async (context,done) => {
+    let url,mail;
+    mail=context.properties().newAdd
+    let usrIP=context.properties().code
+
+
+await fetch(`https://fa-${credImport.server}-saasfademo1.ds-fa.oraclepdemos.com//hcmRestApi/resources/11.13.18.05/emps?q=PersonNumber=`+codeReceived,{
+    method: 'get',
+    headers: {
+        'Authorization': 'Basic '+btoa(credImport.userName+':'+credImport.password),
+    },
+})
+.then(response=>response.json())
+.then(async json=>{
+  ////////////////////
+  //code for patch
+  console.log(json)
+  url=json.items[0].links[0].href
+  console.log(url)
+
+
+var raw = JSON.stringify({
+    "WorkEmail": mail,
+  });
+
+await fetch(url,{
+    method: 'PATCH',
+    headers: {
+        'Authorization': 'Basic '+btoa(credImport.userName+':'+credImport.password),
+        'Content-type': 'application/json'
+    },
+    body: raw,
+})
+.then(response=>response.json())
+.then(json=>{
+  console.log(json)
+  context.transition("success")
+  context.keepTurn(true);
+  done();
+})
+.catch(error=>{
+  console.log(error)
+  context.transition("fail")
+  context.keepTurn(true);
+  done();
+})
+})
+/////////////////////////endpatch
+.catch(error=>{
+  console.log(json)
+  context.reply('fail')
+  context.transition("fail")
+  context.keepTurn(true);
+  done();
+})
+}
+};
